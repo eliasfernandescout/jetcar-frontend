223e5766eb7a17a5e3e0e3ccb5a17c3d7db97061
eliasfernandescout <eliasfernandescout@gmail.com>
2022-05-15 17:12:47 -0300

    remove trash and add new component

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,8 @@
+export default function HomePage(){
+    return(
+        <div>
+            <h1>Bem vindo a Jet Car</h1>
+            <h2>Alugue seu veículo por um dos melhores preços</h2>
+        </div>
+    )
+}

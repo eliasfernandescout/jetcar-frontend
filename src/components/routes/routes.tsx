bfb4df443ee9d29e7e06f178979e5a7e8bd69d99
eliasfernandescout <eliasfernandescout@gmail.com>
2022-06-11 20:55:19 -0300

    refactor: update routes path

--- src/components/routes/routes.tsx
+++ src/components/routes/routes.tsx
@@ -1,14 +1,27 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from '../../pages/home/home'
 
-export const RoutesComponent = () => {
-  return (
-    <main>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/bye' element={<div>Tachau</div>}/>
-      </Routes>
-    </main>
+// export const RoutesComponent = () => {
+//   return (
+//     <main>
+//       <Routes>
+//         <Route path='/' element={<Home/>}/>
+//         <Route path='/bye' element={<div>Tachau</div>}/>
+//       </Routes>
+//     </main>
 
-  )
+//   )
+// }
+
+export enum RoutePath{
+  LOG_IN='/login',
+  FORGOT_PASSWORD = '/forgot-password',
+  HOME='/',
+  MESSAGES='/messages',
+  INBOX='/inbox',
+  NOTIFICATIONS='/notifications',
+  SETTINGS='/settings',
+  CREATE_MESSAGE='/create-message',
+  REMOVED_MESSAGES='/removed-messages',
+  LOG_OUT='/logout'
 }

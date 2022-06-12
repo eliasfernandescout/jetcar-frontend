c3e818b4d55d93213c82c13b0845e0ff54e40453
eliasfernandescout <eliasfernandescout@gmail.com>
2022-06-12 17:30:20 -0300

    chore: add default export

---
+++ src/pages/index.ts
@@ -0,0 +1,17 @@
+import Home from './Home'
+import Logout from './Logout'
+import Notifications from './Notifications'
+import CarRegistration from './cars/CarRegistration'
+import Cars from './cars/Cars'
+import RentedCars from './cars/RentedCars'
+import { NoMatch } from './NoMatch'
+
+export {
+  Home,
+  Logout,
+  Notifications,
+  CarRegistration,
+  Cars,
+  RentedCars,
+  NoMatch
+}

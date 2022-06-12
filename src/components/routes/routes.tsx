e74336e0ddedb81ecc28bdc81790a58e67502e7e
eliasfernandescout <eliasfernandescout@gmail.com>
2022-06-11 23:14:59 -0300

    feat: add default sidebar nav and message sidebarnav

--- src/components/routes/routes.tsx
+++ src/components/routes/routes.tsx
@@ -1,5 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
+import React from 'react'
 import Home from '../../pages/home/home'
+import { MdHome, MdNotifications, MdEmail, MdSettings, MdLogout, MdOutlineCreate, MdDeleteForever } from 'react-icons/md'
 
 // export const RoutesComponent = () => {
 //   return (
@@ -24,4 +26,71 @@ export enum RoutePath{
   CREATE_MESSAGE='/create-message',
   REMOVED_MESSAGES='/removed-messages',
   LOG_OUT='/logout'
+}
+
+export const defaultSidebarNavIems: SidebarNavItem[] = [
+  {
+    name: 'home',
+    path: RoutePath.HOME,
+    icon: MdHome
+  },
+  {
+    name: 'notifications',
+    path: RoutePath.NOTIFICATIONS,
+    icon: MdNotifications
+  },
+  {
+    name: 'inbox',
+    path: RoutePath.MESSAGES,
+    icon: MdEmail
+  },
+  {
+    name: 'settings',
+    path: RoutePath.SETTINGS,
+    icon: MdSettings
+  },
+  {
+    name: 'logout',
+    path: RoutePath.LOG_OUT,
+    icon: MdLogout
+  }
+
+]
+
+export const messageSidebarNavItems: SidebarNavItem[] = [
+  {
+    name: 'home',
+    path: RoutePath.HOME,
+    icon: MdHome
+  },
+  {
+    name: 'Create message',
+    path: RoutePath.CREATE_MESSAGE,
+    icon: MdOutlineCreate
+
+  },
+  {
+    name: 'Check message',
+    path: RoutePath.MESSAGES,
+    icon: MdEmail
+  },
+  {
+    name: 'Removed messages',
+    path: RoutePath.REMOVED_MESSAGES,
+    icon: MdDeleteForever
+  }
+]
+
+export interface SidebarNavItem {
+  name: string
+  path: string
+  icon: React.ComponentType
+}
+
+export interface Route {
+  name: string
+  path: RoutePath
+  SidebarNavItems?: SidebarNavItem[]
+  component: React.ComponentType
+
 }

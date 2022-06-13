1154c86c736dca6bfa81f2cb0fd2b9771b814a78
eliasfernandescout <eliasfernandescout@gmail.com>
2022-06-12 21:07:05 -0300

    feat: add global style

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,30 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { RoutePath, routes } from 'components/routes/routes'
 import { NoMatch } from 'pages'
 import Layout from 'components/layout/Layout'
+import { GlobalStyle } from 'styles/global'
 
 function App () {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout/>}>
-          {routes.map((route) => (
-            <Route
-              index={RoutePath.HOME === route.path}
-              path={route.path}
-              element={<route.component/>}
-              key={route.name}
-            />
-          ))}
-        </Route>
-        <Route path='*' element={<NoMatch/>}/>
+    <>
+      <GlobalStyle/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout/>}>
+            {routes.map((route) => (
+              <Route
+                index={RoutePath.HOME === route.path}
+                path={route.path}
+                element={<route.component/>}
+                key={route.name}
+              />
+            ))}
+          </Route>
+          <Route path='*' element={<NoMatch/>}/>
 
-      </Routes>
+        </Routes>
 
-    </BrowserRouter>
+      </BrowserRouter>
+    </>
   )
 }
 

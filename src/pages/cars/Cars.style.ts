4fc80ef35cc82a5dbf5e698c2afe1d7bcff34a46
eliasfernandescout <eliasfernandescout@gmail.com>
2022-06-15 11:01:23 -0300

    feat: create Cars and style cars components

--- src/pages/cars/Cars.style.ts
+++ src/pages/cars/Cars.style.ts
@@ -1,5 +1,15 @@
 import styled from 'styled-components'
 
-export const Cart = styled.nav`
-    grid-area: sidebar;
+export const CarsList = styled.ul`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 10px;
+    padding: 20px;
+    max-width: 1200px;
+    margin: auto;
+`
+
+export const Title = styled.h2`
+    text-align: center;
+    margin-top: 20px;
 `
--- src/pages/cars/Cars.tsx
+++ src/pages/cars/Cars.tsx
@@ -1,1 +1,33 @@
-export default () => <div>Veículos Registrados</div>
+import React, { useEffect, useState } from 'react'
+import { loadCars } from 'services/load-cars.service'
+import CarItem from './components/CarItem'
+import { CarModel } from '../../model/CarModel'
+
+import * as S from './Cars.style'
+
+const AllCars = () => {
+  const [cars, setCars] = useState<CarModel[]>([])
+
+  const getCars = async () => {
+    const response = await loadCars()
+    setCars(response.data)
+  }
+
+  useEffect(() => {
+    getCars()
+  }, [])
+
+  return (
+    <>
+      <S.Title>Carros Cadastrados</S.Title>
+      <S.CarsList>
+
+        {cars.map((car) => (
+          <CarItem car={car} key={car.licensePlate}/>
+        ))}
+      </S.CarsList>
+    </>
+  )
+}
+
+export default AllCars

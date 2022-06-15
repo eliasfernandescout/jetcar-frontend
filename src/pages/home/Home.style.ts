1691e04509ce4dc2cd4cd41c0faec03dacdb8dff
eliasfernandescout <eliasfernandescout@gmail.com>
2022-06-15 11:00:34 -0300

    refactor: remove home style

--- src/pages/home/Home.style.ts
+++ src/pages/home/Home.style.ts
@@ -1,31 +1,10 @@
 import styled from 'styled-components'
 
-export const Form = styled.form`
+export const Form = styled.div`
     padding: 20px;
     background-color: #333;
     max-width: 500px;
     border-radius: 3px;
     color: #eee;
     margin: 50px auto;
-`
-
-export const FormTitle = styled.h2`
-    margin-bottom: 20px;
-`
-
-export const Input = styled.input`
-    width: 100%;
-    background-color: #555;
-    padding: 8px 5px;
-    border: 1px solid #444;
-    margin-bottom: 10px;
-    border-radius: 3px;
-`
-
-export const Button = styled.button`
-    background-color: orangered;
-    color: #eee;
-    border-radius: 3px;
-    padding: 8px 15px;
-    border: none;
 `

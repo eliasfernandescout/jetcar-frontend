2ba38cdec7496a9f9f9ae31908936ee6a0079191
eliasfernandescout <eliasfernandescout@gmail.com>
2022-06-14 21:00:09 -0300

    chore: add try catch for return request status

--- src/services/car-registration.service.ts
+++ src/services/car-registration.service.ts
@@ -1,13 +1,17 @@
 import axios from 'axios'
 
 export const carRegistration = async (formData: FormData) => {
-  const response = await axios({
-    method: 'POST',
-    url: 'http://localhost:5002/car/car-register',
-    data: formData,
-    headers: {
-      'Context-Type': 'multipart/form-data'
-    }
-  })
-  return response
+  try {
+    const response = await axios({
+      method: 'POST',
+      url: 'http://localhost:5002/car/car-register',
+      data: formData,
+      headers: {
+        'Context-Type': 'multipart/form-data'
+      }
+    })
+    return response
+  } catch (error) {
+    return { status: 400 }
+  }
 }
